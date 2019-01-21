388c424a1599dbff61ae3b04faf9f089656dc16c
Rajesh Nanjarapalli <nanjarapalli@gmail.com>
2019-01-21 20:11:54 +0100

    Reworked employee report page

--- application/views/employeereport.php
+++ application/views/employeereport.php
@@ -5,166 +5,108 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
-    <link  href=" https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"></link>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style>
-	h1{
-    margin-top: -40px;
-    margin-bottom: 10px;
-}
-  
-	.Back{
-        
-		right: 3px;
-		padding : 10px;
-		text-align: right;
-		display: inline-block;
-		width: 100%;
-		margin-right: -50%;
-	}
-	.feed1 {
-  background-color : #31B0D5;
-  color: white;
-  padding: 10px 20px;
-  border-radius: 4px;
-  border-color: #46b8da;
-}
+		<link  href=" https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+			<link href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"></link>
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+				<style type="text/css">
+					.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;margin:0px auto;border: 5px solid #26ADE4;border-radius: 10px;}
+					.tg td{font-family:Arial, sans-serif;font-size:14px;padding:5px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
+					.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:20px 20px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#329a9d;}
+					.tg .tg-0pky{border-color:inherit;text-align:center;vertical-align:center}
+					.tg .tg-28l8{background-color:#ffffff;border-color:inherit;text-align:center}
+					@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}
+					#container {
+					width: 100%;
+					color: #1e73b9;
+					height: auto;
+					padding:30px;
+					margin:auto;
+					}
+				</style>
+			</head>
+			<body>
+				<?php $id = intval($_GET['id']);?>
+				<?php $emploeeuserid = $_SESSION['emploeeuserid'];  ?>
+				<?php if($id == $emploeeuserid){?>
+				<div id="container">
+					<div class="tg-wrap"><table class="tg">
+							<tr>
+								<th colspan="10" style="background-color:#ffffff;">
+									<button onclick="window.location.href='<?php echo base_url(); ?>Welcome/weekdaysforincrement'" style="text-align: center;" type="button" class="btn btn-success">Next</button>
+									<select name="filter" style="height: 34px;background-color:#26ADE4;border-radius: 5px;">
+										<option value="Week">Week</option>
+										<option value="Month">Month</option>
+										<option value="Year">Year</option>
+									</select>
+									<button onclick="window.location.href='<?php echo base_url(); ?>Welcome/weekdaysfordecrement'" style="text-align: center;" type="button" class="btn btn-success">Previous</button>
+									<button onclick="window.location.href='<?php echo base_url(); ?>Welcome/ExportCSV'" style="text-align: center;" type="button" class="btn btn-success">Export</button>
+								</th>
+							</tr>
+							<tr>
+								<th class="tg-0pky">Date</th>
+								<th class="tg-0pky">Start Time</th>
+								<th class="tg-0pky">Break</th>
+								<th class="tg-0pky">End Time</th>
+								<th class="tg-0pky">Rounding</th>
+								<th class="tg-0pky">Total</th>
+								<th class="tg-0pky">Project</th>
+								<th class="tg-0pky">Category</th>
+								<th class="tg-0pky">Comments</th>
+								<th class="tg-0pky">Action</th>
+							</tr>
+							<tr>
+								<?php $num = 1;?>
+								<?php foreach($viewdata as $data){ ?>
+								<?php if($data->empid == $emploeeuserid ){?>
+								<td class="tg-0pky"><?php echo $data->date;?></td>
+								<td class="tg-0pky"><?php echo $data->strattime;?></td>
+								<td class="tg-0pky">
+									<select name="D1">
+										<option value="0:30">0:30</option>
+										<option value="1:00">1:00</option>
+										<option value="1:30">1:30</option>
+										<option value="2:00">2:00</option>
+									</select>
+								</td>
+								<td class="tg-0pky"><?php echo $data->endtime;?></td>
+								<td class="tg-0pky"><?php echo $data->rounding;?></td>
+								<td class="tg-0pky"><?php echo $data->total;?></td>
+								<td class="tg-0pky"><?php echo $data->project;?></td>
+								<td class="tg-0pky"><?php echo $data->cat;?></td>
+								<td class="tg-0pky">
+									<textarea name="message" rows="2" cols="70">
+										<?php echo $data->workdetails;?>
+									</textarea>
+								</td>
+								<td class="tg-0pky">
+									<button style="text-align: center;" type="button" class="btn btn-success">Edit</button>
+								</td>
+								<?php $num ++;?>
+								<tr/>
+								<?php } ?>
+								<?php } ?>
+							</tr>
+						</table></div>
+				</div>
+			</body>
+			<?php } else{?>
+			<?php redirect(base_url() ); ?>
+			<?php } ?>
+			<script>
+				var $rows = $('#table tr');
+				$('#search').keyup(function() {
 
-#mybutton1 {
-  position: fixed;
-  bottom: -4px;
-  left: 10px;
-}
-     
-      .feed {
-  background-color : #31B0D5;
-  color: white;
-  padding: 10px 20px;
-  border-radius: 4px;
-  border-color: #46b8da;
-}
+				var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+				reg = RegExp(val, 'i'),
+				text;
 
-#mybutton {
-  position: fixed;
-  bottom: -4px;
-  right: 10px;
-}
-	tr:nth-child(even) {
-	  background-color: #5bc0de;
-	}  
-   
-	</style>
-	</head>
-	<body>
-  <?php $id = intval($_GET['id']);?>
-
-<?php
-
-$emploeeuserid = $_SESSION['emploeeuserid'];  
-?>
-<?php
-
-
-
-// echo "Current week range from $this_week_sd to $this_week_ed ";
-?>
-
-<?php if($id == $emploeeuserid){?>
-	<div class="container">
-			<div class="Back">
-      <button onclick="window.location.href='<?php echo base_url();?>Welcome/createmonthtable'" style="text-align: center;" type="button" class="btn btn-success">Month Table</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Welcome/monthdays'" style="text-align: center;" type="button" class="btn btn-success">Month</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Welcome/weekdays'" style="text-align: center;" type="button" class="btn btn-success">Week</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Managementcontrol/Conformorder?id='" style="text-align: center;" type="button" class="btn btn-success">Year</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Welcome/weekdaysforincrement'" style="text-align: center;" type="button" class="btn btn-success">Next</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Managementcontrol/Conformorder?id='" style="text-align: center;" type="button" class="btn btn-success">Add Entry</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Welcome/weekdaysfordecrement'" style="text-align: center;" type="button" class="btn btn-success">Previous</button>
-            <button onclick="window.location.href='<?php echo base_url(); ?>Welcome/ExportCSV'" style="text-align: center;" type="button" class="btn btn-success">Export</button>
-            <INPUT TYPE="button" style="text-align: center" type="button" class="btn btn-success" VALUE="Back" onClick="history.go(-1);">
-                    </div>
-        <h1><b>Report</b></h1>
-			<div class="form-group">
-			
-			</div>
-			<br />
-	
-<input type="text"  align="center" border="1px" style="width:100%; line-height: 30px;" id="search" placeholder=" Search Here">
-<table id="table" class="table" align="center" border="1px" style="width:100%; line-height: 30px;">    
-            
-            
-        <tr>
-             <th>S.NO</th>
-            <th>Date</th>
-            <th>Start Time</th> 
-              <th>Break Time</th>
-               <th>End Time</th>
-               <th>Rounding </th>
-               
-               <th>Total</th>
-                <th>Customer</th>
-                  <th>Project</th>
-                   <th>cat</th>
-                    <th>Work details</th>
-            
-           
-            
-             </tr>
-              <tbody >							
-    
-	 
-                
-<tr>
-<?php $num = 1;?>
-				<?php foreach($viewdata as $data){ ?>
-    <?php if($data->empid == $emploeeuserid ){?>
-	<td> <?php echo $num;?> </td>
-	<td><?php echo $data->date; ?> </td>
-         <td><?php echo $data->strattime; ?> </td>
-         <td> <?php echo $data->breaks;?></td>
-	<td><?php echo $data->endtime; ?> </td>
-	<td><?php echo $data->rounding; ?> </td>
-        <td><?php echo $data->total; ?> </td>
-        
-         <td><?php echo $data->custome; ?> </td>
-          <td><?php echo $data->project; ?> </td>
-           <td><?php echo $data->cat; ?> </td>
-            <td><?php echo $data->workdetails; ?> </td>
-            <?php $num ++;?>
-                                        
-                                       
-                                        <tr/>
-					<?php } ?>
-                                        <?php } ?>
-					<tr>
-
-				
-
-
-				</tbody>  
-</table>
-</div>
-
-	</body>
-            <?php } else{?>
-        
-         <?php redirect(base_url() ); ?>
-            <?php } ?>
-         <script>
-                   var $rows = $('#table tr');
-$('#search').keyup(function() {
-    
-    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-        reg = RegExp(val, 'i'),
-        text;
-    
-    $rows.show().filter(function() {
-        text = $(this).text().replace(/\s+/g, ' ');
-        return !reg.test(text);
-    }).hide();
-});
-         </script>
-</html>
+				$rows.show().filter(function() {
+				text = $(this).text().replace(/\s+/g, ' ');
+				return !reg.test(text);
+				}).hide();
+				});
+			</script>
+		</html>
